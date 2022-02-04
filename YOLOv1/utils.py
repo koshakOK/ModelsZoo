50599302325b7c00c9b6ad137b0acedc321cad23
Клещев Максим <MKleshchev@sberbank.ru>
2022-02-04 23:55:21 +0300

    adds

--- YOLOv1/utils.py
+++ YOLOv1/utils.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import Counter
+import matplotlib.patches as patches
 import torchvision
 
 EPS = 1e-6
@@ -130,3 +131,36 @@ def mean_average_precision(pred_boxes,
         average_precisions.append(torch.trapz(precisions, recalls))
 
     return torch.mean(average_precisions)
+
+
+def plot_image(image, boxes):
+    """Plots predicted bounding boxes on the image"""
+    im = np.array(image)
+    height, width, _ = im.shape
+
+    # Create figure and axes
+    fig, ax = plt.subplots(1)
+    # Display the image
+    ax.imshow(im)
+
+    # box[0] is x midpoint, box[2] is width
+    # box[1] is y midpoint, box[3] is height
+
+    # Create a Rectangle potch
+    for box in boxes:
+        box = box[2:]
+        assert len(box) == 4, "Got more values than in x, y, w, h, in a box!"
+        upper_left_x = box[0] - box[2] / 2
+        upper_left_y = box[1] - box[3] / 2
+        rect = patches.Rectangle(
+            (upper_left_x * width, upper_left_y * height),
+            box[2] * width,
+            box[3] * height,
+            linewidth=1,
+            edgecolor="r",
+            facecolor="none",
+        )
+        # Add the patch to the Axes
+        ax.add_patch(rect)
+
+    plt.show()
